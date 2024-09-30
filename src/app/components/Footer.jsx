d70fd2d8ae51a1234c72acd756a8762a533aae62
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2024-09-30 16:07:20 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import { motion } from 'framer-motion'; // For React animations
+// For React animations
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
