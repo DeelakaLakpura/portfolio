a9ebccdd958558ccc26cc8e042cae7ffed982c80
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2024-10-01 18:43:27 +0530

    page update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,11 @@
-"use client";
-import { useState, useEffect } from "react"; // Import useState and useEffect
 import ProfileCard from "./components/ProfileCard";
 import ProjectShowcase from "./components/ProjectShowcase";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
 import TestomonialSection from "./components/TestimonialSection";
-import ScrollToTopButton from "./components/ScrollToTopButton";
-import Loading from "./components/Loading"; // Import the Loading component
+import ScrollToTopButton from "./components/ScrollToTopButton"; 
 
 export default function Home() {
-  const [loading, setLoading] = useState(true); // Create loading state
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false); // Set loading to false after 2 seconds (or any desired time)
-    }, 2000); // Adjust the duration as needed
-
-    return () => clearTimeout(timer); // Cleanup the timer
-  }, []);
-
-  if (loading) {
-    return <Loading />; // Show the loading screen if loading is true
-  }
-
   return (
     <div className="min-h-screen bg-gray-900 text-white flex flex-col lg:flex-row justify-between items-start p-3 md:p-3 lg:p-6 overflow-x-hidden">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
@@ -45,9 +28,12 @@ export default function Home() {
         {/* Full width footer */}
         <div className="w-full mt-6 bg-gray-900">
           <Footer />
+      
         </div>
+     
       </div>
       <ScrollToTopButton />
     </div>
+
   );
 }
