182addcb12108c71c8b57dfe985d907698ee9a27
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2024-10-01 19:01:07 +0530

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c42acc0d42aae3c7"; }
+/******/ 		__webpack_require__.h = function() { return "cb54be57d354e4ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
